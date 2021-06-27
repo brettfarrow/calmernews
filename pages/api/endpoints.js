0bdc57d9164f01a8791f7437ce2e3e8c6c55de36
Brett Farrow <brett@acorncity.com>
2021-06-26 19:26:11 -0600

    Formatted endpoints

--- pages/api/endpoints.js
+++ pages/api/endpoints.js
@@ -1,13 +1,13 @@
-const SITE_HOSTNAME = 'https://news.ycombinator.com'
+const SITE_HOSTNAME = "https://news.ycombinator.com";
 const endpoints = {
-    HOME: SITE_HOSTNAME,
-    NEWS: `${SITE_HOSTNAME}/news`,
-    NEWEST: `${SITE_HOSTNAME}/newest`,
-    ASK: `${SITE_HOSTNAME}/ask`,
-    SHOW: `${SITE_HOSTNAME}/show`,
-    FROM: `${SITE_HOSTNAME}/from`,
-    USER: `${SITE_HOSTNAME}/user`,
-    COMMENTS: `${SITE_HOSTNAME}/item`,
-}
+  HOME: SITE_HOSTNAME,
+  NEWS: `${SITE_HOSTNAME}/news`,
+  NEWEST: `${SITE_HOSTNAME}/newest`,
+  ASK: `${SITE_HOSTNAME}/ask`,
+  SHOW: `${SITE_HOSTNAME}/show`,
+  FROM: `${SITE_HOSTNAME}/from`,
+  USER: `${SITE_HOSTNAME}/user`,
+  COMMENTS: `${SITE_HOSTNAME}/item`,
+};
 
-export default endpoints
+export default endpoints;
