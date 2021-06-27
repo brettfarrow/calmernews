9146fe242f676bfe30a89a5e5160e82e74da1284
Brett Farrow <brett@acorncity.com>
2021-06-26 19:19:52 -0600

    Added HN endpoints

---
+++ pages/api/endpoints.js
@@ -0,0 +1,13 @@
+const MAIN_DOMAIN = 'https://news.ycombinator.com'
+const endpoints = {
+    HOME: MAIN_DOMAIN,
+    NEWS: `${MAIN_DOMAIN}/news`,
+    NEWEST: `${MAIN_DOMAIN}/newest`,
+    ASK: `${MAIN_DOMAIN}/ask`,
+    SHOW: `${MAIN_DOMAIN}/show`,
+    FROM: `${MAIN_DOMAIN}/from`,
+    USER: `${MAIN_DOMAIN}/user`,
+    COMMENTS: `${MAIN_DOMAIN}/item`,
+}
+
+export default endpoints
