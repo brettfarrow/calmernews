9b92c4d68e6f7112bcb9604453052e344582382d
Brett Farrow <brett@acorncity.com>
2021-06-26 22:32:09 -0600

    Added cookies to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,26 @@
 import fetch from 'isomorphic-fetch';
 import Post from '../components/Post';
 
-function Index({ data }) {
+function Index({ data, cookies }) {
   return (
     <div>
       <h1>hnnc</h1>
       <ol>
         {data.items.map((post, index) => (
-          <Post post={post} index={index} />
+          <Post post={post} key={index} index={index} />
         ))}
       </ol>
     </div>
   );
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps(ctx) {
   const data = await fetch(`${process.env.HOST}/api`).then((r) => r.json());
   return {
-    props: { data },
+    props: {
+      data,
+      cookies: ctx.req.cookies,
+    },
   };
 }
 
