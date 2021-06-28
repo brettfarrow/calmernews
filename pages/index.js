e8847e024b97edd6c34f0c8a1ba8b5ac22e1df46
Brett Farrow <brett@acorncity.com>
2021-06-27 22:44:40 -0600

    Added prefetch

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import get from 'lodash/get';
 import React, { useState } from 'react';
 import Posts from '../components/Posts';
 import Link from 'next/link';
+import { Prefetch } from '@layer0/react';
 
 function Index({ data, cookies }) {
   const [showComments, setShowComments] = useState(
@@ -32,7 +33,9 @@ function Index({ data, cookies }) {
           <button
             className={`rounded flex content-center justify-self-center text-center bg-purple-600 w-24 h-12 mb-6 text-white font-bold py-3 px-7`}
           >
-            <a href={get(data, 'more', '')}>More</a>
+            <Prefetch>
+              <a href={get(data, 'more', '')}>More</a>
+            </Prefetch>
           </button>
         </Link>
       </div>
