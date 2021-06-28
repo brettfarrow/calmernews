cb72ce6b07694e01cdb5b14216064d4f14549911
Brett Farrow <brett@acorncity.com>
2021-06-27 22:02:08 -0600

    Removed host from next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,5 @@ const { withLayer0, withServiceWorker } = require('@layer0/next/config');
 module.exports = withLayer0(
   withServiceWorker({
     layer0SourceMaps: true,
-    env: {
-      HOST: 'http://localhost:3000', // TODO: process.env.NODE_ENV === 'production' ? `https://example.com/` : `http://localhost:3000`
-    },
   })
 );
