fbea891d47acf4c0f57d02742d19cc48f605a4cc
Brett Farrow <brett@acorncity.com>
2021-06-27 22:02:16 -0600

    Added caching and routes

---
+++ cache.js
@@ -0,0 +1,43 @@
+import { CustomCacheKey } from '@layer0/core/router';
+
+const FIFTEEN_MINUTE_TTL = 60 * 15;
+const ONE_HOUR_TTL = 60 * 60;
+const ONE_DAY_TTL = 60 * 60 * 24;
+const ONE_YEAR_TTL = 60 * 60 * 24 * 365;
+
+const key = new CustomCacheKey().excludeAllQueryParametersExcept(
+  'p',
+  'n',
+  'next'
+);
+
+module.exports = {
+  FIFTEEN_MINUTE_TTL,
+  ONE_HOUR_TTL,
+  ONE_DAY_TTL,
+  ONE_YEAR_TTL,
+  key,
+  NEWS: {
+    key,
+    browser: {
+      maxAgeSeconds: 0,
+    },
+    edge: {
+      maxAgeSeconds: ONE_HOUR_TTL,
+      staleWhileRevalidateSeconds: FIFTEEN_MINUTE_TTL,
+    },
+  },
+  SERVICE_WORKER: {
+    browser: {
+      maxAgeSeconds: 0,
+    },
+    edge: {
+      maxAgeSeconds: ONE_YEAR_TTL,
+      staleWhileRevalidateSeconds: ONE_HOUR_TTL,
+    },
+  },
+  cacheResponse:
+    (config) =>
+    ({ cache }) =>
+      cache(config),
+};
--- routes.js
+++ routes.js
@@ -1,8 +1,14 @@
 const { Router } = require('@layer0/core/router');
 const { nextRoutes } = require('@layer0/next');
+const { NEWS, SERVICE_WORKER, cacheResponse } = require('./cache');
 
 module.exports = new Router()
-  .match('/service-worker.js', ({ serviceWorker }) => {
+  .match('/service-worker.js', ({ cache, serviceWorker }) => {
+    cache(SERVICE_WORKER);
     return serviceWorker('.next/static/service-worker.js');
   })
-  .use(nextRoutes); // automatically adds routes for all files under /pages
+  .match('/', cacheResponse(NEWS))
+  .match('/api', cacheResponse(NEWS))
+  .match('/news', cacheResponse(NEWS))
+  .match('/api/news', cacheResponse(NEWS))
+  .use(nextRoutes);
