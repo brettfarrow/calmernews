974615ecd5a9cd7710aecfe84a52bc3393365229
Brett Farrow <brett@acorncity.com>
2021-07-02 20:06:19 -0600

    Revert "Switched to preact"
    This reverts commit 1d1bf321b74bd62ef956a61570e39f05377abc92.

--- next.config.js
+++ next.config.js
@@ -1,21 +1,7 @@
 const { withLayer0, withServiceWorker } = require('@layer0/next/config');
 
-module.exports = {
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      });
-    }
-
-    return config;
-  },
-  ...withLayer0(
-    withServiceWorker({
-      layer0SourceMaps: true,
-    })
-  ),
-};
+module.exports = withLayer0(
+  withServiceWorker({
+    layer0SourceMaps: true,
+  })
+);
