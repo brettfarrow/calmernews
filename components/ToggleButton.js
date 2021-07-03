a8bac5b036177ce9803f8718b9b7dee47c124fad
Brett Farrow <brett@acorncity.com>
2021-07-02 19:13:59 -0600

    Removed toggle buttons when JS is disabled

--- components/ToggleButton.js
+++ components/ToggleButton.js
@@ -1,6 +1,6 @@
 export default function ToggleButton({ name, value, onClick }) {
   return (
-    <button onClick={onClick} className={`p-4`}>
+    <button onClick={onClick} className={`toggle-button p-4`}>
       <span className={`underline underline-link`}>
         {value ? 'Stop Showing' : 'Show'} {name}
       </span>
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html className="dark" lang="en">
+      <Html className={`dark no-js`} lang="en">
         <Head>
           <title>calmer news</title>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -16,11 +16,12 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
             rel="stylesheet"
           />
+          <script>document.documentElement.classList.remove("no-js");</script>
           <script
             defer
             data-domain="calmernews.com"
             src="https://plausible.io/js/plausible.js"
-          ></script>
+          />
         </Head>
         <body className="bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-100">
           <Main />
