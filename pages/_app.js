ebd09fe6cd1fff42e933af710d04d7bfd7268677
Brett Farrow <brett@acorncity.com>
2021-07-04 22:56:12 -0600

    Added Plausible analytics on subsequent navs

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
 import '../styles/globals.css';
 import PWABoilerplate from '../components/PWABoilerplate';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      setTimeout(() => {
+        if (window?.plausible) window.plausible('pageview');
+      }, 1000);
+
+      router.events.on('routeChangeStart', handleRouteChange);
+
+      // If the component is unmounted, unsubscribe
+      // from the event with the `off` method:
+      return () => {
+        router.events.off('routeChangeStart', handleRouteChange);
+      };
+    };
+  }, []);
+
   return (
     <>
       <PWABoilerplate />
