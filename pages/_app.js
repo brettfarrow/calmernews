727ad6dcd71e58e0b6a7d60379e0fb09133df3f2
Brett Farrow <brett@acorncity.com>
2021-07-04 22:55:52 -0600

    Reduced Google Fonts usage

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,9 @@
 import '../styles/globals.css';
 import PWABoilerplate from '../components/PWABoilerplate';
-import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <PWABoilerplate />
       <Component {...pageProps} />
     </>
