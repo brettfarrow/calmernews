b7a5f5079952d3116fb251e3e658d0d28d98de52
Brett Farrow <brett@acorncity.com>
2021-07-05 00:01:21 -0600

    Added Preact

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 const { withLayer0, withServiceWorker } = require('@layer0/next/config');
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
+const withPreact = require('next-plugin-preact');
 
-module.exports = {
+module.exports = withPreact({
   ...withLayer0(
     withServiceWorker({
       layer0SourceMaps: true,
@@ -14,4 +15,4 @@ module.exports = {
       },
     })
   ),
-};
+});
