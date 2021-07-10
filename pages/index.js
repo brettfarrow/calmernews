33f64a1572476e036bab42f815a3b015b36408e7
Brett Farrow <brett@acorncity.com>
2021-07-10 11:43:48 -0600

    Added loading spinner to nav buttons

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import fetch from 'isomorphic-fetch';
 import get from 'lodash/get';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Posts from '../components/Posts';
 import ToggleButton from '../components/ToggleButton';
+import LoadingButton from '../components/LoadingButton';
 import Link from 'next/link';
 import { Prefetch } from '@layer0/react';
 import Head from 'next/head';
@@ -11,6 +12,10 @@ import Script from 'next/script';
 function Index({ data, cookies }) {
   const p = get(data, 'page', 1);
   const { from, more, previous } = data;
+  const [loading, setLoading] = useState({
+    loading: false,
+    button: false,
+  });
   const [showComments, setShowComments] = useState(
     get(cookies, 'show_comments', false) === 'true'
   );
@@ -25,6 +30,14 @@ function Index({ data, cookies }) {
     document.cookie = `${name}=${!value}; Max-Age=2147483647`;
     setter(!value);
   };
+
+  useEffect(() => {
+    if (loading)
+      setLoading({
+        loading: false,
+        button: false,
+      });
+  }, [data]);
 
   return (
     <>
@@ -62,11 +75,24 @@ function Index({ data, cookies }) {
             <div className={`flex justify-center`}>
               <button
                 className={`rounded text-center bg-purple-700 w-28 h-12 m-6 text-white font-bold transition duration-500 ease-in-out hover:bg-purple-800`}
+                onClick={() =>
+                  setLoading({
+                    loading: true,
+                    button: 'previous',
+                  })
+                }
+                disabled={loading.button === false ? false : true}
               >
                 <Link href={previous}>
                   <Prefetch url={`/api${previous}`}>
                     <a className={`w-28 h-12 block leading-12`} href={previous}>
-                      Previous
+                      {loading.loading && loading.button === 'previous' ? (
+                        <LoadingButton />
+                      ) : loading.button === 'more' ? (
+                        ''
+                      ) : (
+                        'Previous'
+                      )}
                     </a>
                   </Prefetch>
                 </Link>
@@ -77,11 +103,24 @@ function Index({ data, cookies }) {
             <div className={`flex justify-center`}>
               <button
                 className={`rounded text-center bg-purple-700 w-28 h-12 m-6 text-white font-bold transition duration-500 ease-in-out hover:bg-purple-800`}
+                onClick={() =>
+                  setLoading({
+                    loading: true,
+                    button: 'more',
+                  })
+                }
+                disabled={loading.button === false ? false : true}
               >
                 <Link href={more}>
                   <Prefetch url={`/api${more}`}>
                     <a className={`w-28 h-12 block leading-12`} href={more}>
-                      More
+                      {loading.loading && loading.button === 'more' ? (
+                        <LoadingButton />
+                      ) : loading.button === 'previous' ? (
+                        ''
+                      ) : (
+                        'More'
+                      )}
                     </a>
                   </Prefetch>
                 </Link>
