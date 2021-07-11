9a6d538496d7310b0bdcb08e639a1bd1193913cb
Brett Farrow <brett@acorncity.com>
2021-07-10 20:14:34 -0600

    Added pull to refresh

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,19 @@ import Link from 'next/link';
 import { Prefetch } from '@layer0/react';
 import Head from 'next/head';
 import Script from 'next/script';
+import PullToRefresh from 'react-simple-pull-to-refresh';
+import { useRouter } from 'next/router';
+import LoadingButton from '../components/LoadingButton';
 
 function Index({ data, cookies }) {
   const p = get(data, 'page', 1);
   const { from, more, previous } = data;
+
   const [loading, setLoading] = useState({
     loading: false,
     button: false,
   });
+
   const [showComments, setShowComments] = useState(
     get(cookies, 'show_comments', false) === 'true'
   );
@@ -26,9 +31,15 @@ function Index({ data, cookies }) {
     get(cookies, 'show_score', false) === 'true'
   );
 
+  const router = useRouter();
+
   const toggleClick = (name, setter, value) => {
     document.cookie = `${name}=${!value}; Max-Age=2147483647`;
     setter(!value);
+  };
+
+  const handleRefresh = () => {
+    setTimeout(() => router.reload(), 0);
   };
 
   useEffect(() => {
@@ -38,6 +49,10 @@ function Index({ data, cookies }) {
         button: false,
       });
   }, [data]);
+
+  const refreshingContent = (
+    <LoadingButton customClasses={`animate-spin w-8 h-8 my-0 mx-auto`} />
+  );
 
   return (
     <>
@@ -52,55 +67,62 @@ function Index({ data, cookies }) {
           content="A modified UI for Hacker News, starting with dark mode and no comments (by default). Not affiliated with Y Combinator or Hacker News in any way."
         />
       </Head>
-      <div className="bg-coolGray-100 dark:bg-coolGray-700">
-        <h1
-          className={`text-gray-800 dark:text-gray-200 flex justify-center p-4`}
-        >
-          <Link href="/">
-            <Prefetch url="/api">
-              <a href="/">calmer news</a>
-            </Prefetch>
-          </Link>
-        </h1>
-        <Posts
-          from={from}
-          items={get(data, 'items', [])}
-          showComments={showComments}
-          showByline={showByline}
-          showScore={showScore}
-          start={get(data, 'start', 1)}
-        />
-        <NavButtons
-          loading={loading}
-          setLoading={setLoading}
-          more={more}
-          previous={previous}
-          p={p}
-        />
-        <footer className={`flex justify-center pb-16`}>
-          <ToggleButton
-            name={'Byline'}
-            value={showByline}
-            onClick={() => {
-              toggleClick('show_byline', setShowByline, showByline);
-            }}
+      <PullToRefresh
+        pullingContent={''}
+        onRefresh={handleRefresh}
+        refreshingContent={refreshingContent}
+      >
+        <div className="bg-coolGray-100 dark:bg-coolGray-700">
+          <h1
+            className={`text-gray-800 dark:text-gray-200 flex justify-center p-4`}
+          >
+            <Link href="/">
+              <Prefetch url="/api">
+                <a href="/">calmer news</a>
+              </Prefetch>
+            </Link>
+          </h1>
+          <Posts
+            from={from}
+            items={get(data, 'items', [])}
+            showComments={showComments}
+            showByline={showByline}
+            showScore={showScore}
+            start={get(data, 'start', 1)}
           />
-          <ToggleButton
-            name={'Comments'}
-            value={showComments}
-            onClick={() => {
-              toggleClick('show_comments', setShowComments, showComments);
-            }}
+          <NavButtons
+            loading={loading}
+            setLoading={setLoading}
+            more={more}
+            previous={previous}
+            p={p}
+            router={router}
           />
-          <ToggleButton
-            name={'Score'}
-            value={showScore}
-            onClick={() => {
-              toggleClick('show_score', setShowScore, showScore);
-            }}
-          />
-        </footer>
-      </div>
+          <footer className={`flex justify-center pb-16`}>
+            <ToggleButton
+              name={'Byline'}
+              value={showByline}
+              onClick={() => {
+                toggleClick('show_byline', setShowByline, showByline);
+              }}
+            />
+            <ToggleButton
+              name={'Comments'}
+              value={showComments}
+              onClick={() => {
+                toggleClick('show_comments', setShowComments, showComments);
+              }}
+            />
+            <ToggleButton
+              name={'Score'}
+              value={showScore}
+              onClick={() => {
+                toggleClick('show_score', setShowScore, showScore);
+              }}
+            />
+          </footer>
+        </div>
+      </PullToRefresh>
       <Script
         src="/js/measure.js"
         data-domain="calmernews.com"
