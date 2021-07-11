a4733777dbdd98362446c0fe95d6898608f2e285
Brett Farrow <brett@acorncity.com>
2021-07-10 20:06:40 -0600

    Added back button to nav buttons

--- components/NavButtons.js
+++ components/NavButtons.js
@@ -2,9 +2,27 @@ import LoadingButton from './LoadingButton';
 import Link from 'next/link';
 import { Prefetch } from '@layer0/react';
 
-export default function NavButtons({ loading, setLoading, more, previous, p }) {
+export default function NavButtons({
+  loading,
+  setLoading,
+  more,
+  previous,
+  p,
+  router,
+}) {
+  // Show the back button except on page 1 of home
+  const { pathname, asPath } = router;
+  const showBackButton =
+    !more || (more && pathname !== '/' && asPath !== '/news?p=1' && p === 1);
+
+  // One column for More on the homepage
+  // One column for Back when only one page of results
+  // Otherwise, two columns for all pages
+  const gridCols =
+    p === 1 && !showBackButton ? 1 : p === 1 && !more && showBackButton ? 1 : 2;
+
   return (
-    <div className={`grid ${p > 1 ? 'grid-cols-2' : 'grid-cols-1'}`}>
+    <div className={`grid grid-cols-${gridCols}`}>
       {p > 1 && (
         <div className={`flex justify-center`}>
           <button
@@ -21,7 +39,9 @@ export default function NavButtons({ loading, setLoading, more, previous, p }) {
               <Prefetch url={`/api${previous}`}>
                 <a className={`w-28 h-12 block leading-12`} href={previous}>
                   {loading.loading && loading.button === 'previous' ? (
-                    <LoadingButton />
+                    <LoadingButton
+                      customClasses={`animate-spin w-8 h-8 mx-10 my-2 text-white`}
+                    />
                   ) : loading.button === 'more' ? (
                     ''
                   ) : (
@@ -30,6 +50,31 @@ export default function NavButtons({ loading, setLoading, more, previous, p }) {
                 </a>
               </Prefetch>
             </Link>
+          </button>
+        </div>
+      )}
+      {showBackButton && (
+        <div className={`flex justify-center`}>
+          <button
+            className={`rounded text-center bg-purple-700 w-28 h-12 m-6 text-white font-bold transition duration-500 ease-in-out hover:bg-purple-800`}
+            onClick={() => {
+              setLoading({
+                loading: true,
+                button: 'back',
+              });
+              router.back();
+            }}
+            disabled={loading.button === false ? false : true}
+          >
+            {loading.loading && loading.button === 'back' ? (
+              <LoadingButton
+                customClasses={`animate-spin w-8 h-8 mx-10 my-2 text-white`}
+              />
+            ) : loading.button === 'previous' ? (
+              ''
+            ) : (
+              'Back'
+            )}
           </button>
         </div>
       )}
@@ -49,7 +94,9 @@ export default function NavButtons({ loading, setLoading, more, previous, p }) {
               <Prefetch url={`/api${more}`}>
                 <a className={`w-28 h-12 block leading-12`} href={more}>
                   {loading.loading && loading.button === 'more' ? (
-                    <LoadingButton />
+                    <LoadingButton
+                      customClasses={`animate-spin w-8 h-8 mx-10 my-2 text-white`}
+                    />
                   ) : loading.button === 'previous' ? (
                     ''
                   ) : (
