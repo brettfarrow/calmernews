3c7e01b420d0f25864aa3dbf4f59d990c8dfc27f
Brett Farrow <brett@acorncity.com>
2022-05-05 21:26:34 -0600

    Switched back to React

--- next.config.js
+++ next.config.js
@@ -1,18 +1,15 @@
 const { withLayer0, withServiceWorker } = require('@layer0/next/config');
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
-const withPreact = require('next-plugin-preact');
 
-module.exports = withPreact({
-  ...withLayer0(
-    withServiceWorker({
-      layer0SourceMaps: true,
-    }),
-    withPWA({
-      pwa: {
-        dest: 'public',
-        runtimeCaching,
-      },
-    })
-  ),
-});
+module.exports = withLayer0(
+  withServiceWorker({
+    layer0SourceMaps: true,
+  }),
+  withPWA({
+    pwa: {
+      dest: 'public',
+      runtimeCaching,
+    },
+  })
+);
