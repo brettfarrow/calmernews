15e2ca9c05b21926f7dc5eb8225092047b003aeb
Brett Farrow <brett@acorncity.com>
2022-06-01 21:49:41 -0600

    v1.2.4

--- pages/from.js
+++ pages/from.js
@@ -1,19 +1,32 @@
 import fromDomain from './index';
+import get from 'lodash/get';
 
-export async function getServerSideProps(ctx) {
-  const { url } = ctx.req;
-  const data = await fetch(
-    `${process.env.HOST}/api/from?${url.split('?')[1]}`
-  ).then((r) => r.json());
-  return {
-    props: {
+fromDomain.getInitialProps = async function (ctx) {
+  if (ctx.req) {
+    const { url } = ctx.req;
+    const data = await fetch(
+      `${process.env.HOST}/api/from?${url.split('?')[1]}`
+    ).then((r) => r.json());
+    return {
       data: {
         ...data,
         from: true,
       },
       cookies: ctx.req.cookies,
-    },
-  };
-}
+    };
+  } else {
+    const site = get(ctx, 'query.site');
+    const data = await fetch(`/api/from?site=${site}`).then((r) => r.json());
+    const cookies = document.cookie.split('; ').reduce((prev, current) => {
+      const [name, ...value] = current.split('=');
+      prev[name] = value.join('=');
+      return prev;
+    }, {});
+    return {
+      data,
+      cookies,
+    };
+  }
+};
 
 export default fromDomain;
