0ead5dde01b119420ddf98138e1bad8f96d2323b
Brett Farrow <brett@acorncity.com>
2022-06-12 19:15:00 -0600

    Added subreddit page

---
+++ pages/r/[subreddit].js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Page from '../../components/Page';
+import fetch from 'isomorphic-fetch';
+
+function Subreddit({ data, cookies }) {
+  return <Page data={data} cookies={cookies} />;
+}
+
+Subreddit.getInitialProps = async (ctx) => {
+  if (ctx.req) {
+    const { url } = ctx.req;
+    const data = await fetch(`${process.env.HOST}/api/${url}`).then((r) =>
+      r.json()
+    );
+    return {
+      data,
+      cookies: ctx.req.cookies,
+    };
+  } else {
+    const data = await fetch(`/api${ctx.asPath}`).then((r) => r.json());
+    const cookies = document.cookie.split('; ').reduce((prev, current) => {
+      const [name, ...value] = current.split('=');
+      prev[name] = value.join('=');
+      return prev;
+    }, {});
+    return {
+      data,
+      cookies,
+    };
+  }
+};
+
+export default Subreddit;
