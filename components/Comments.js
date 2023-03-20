3f948abed80f9f6c10661e919e753843ebf3fc17
Brett Farrow <brett@acorncity.com>
2023-03-19 23:43:25 -0600

    Comment padding hotfix

--- components/Comments.js
+++ components/Comments.js
@@ -103,7 +103,7 @@ const Comments = ({ data }) => {
               calmer news
             </Link>
           </h1>
-          <div className="max-w-4xl mx-auto">
+          <div className="max-w-4xl mx-auto p-4">
             <h2>
               <a
                 href={link}
