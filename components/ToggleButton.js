4c177fc3c9098dfd86051eaeab80d0726e529f3c
Brett Farrow <brett@acorncity.com>
2023-03-28 21:07:07 -0600

    updated capitalization

--- components/ToggleButton.js
+++ components/ToggleButton.js
@@ -2,7 +2,7 @@ export default function ToggleButton({ name, value, onClick }) {
   return (
     <button onClick={onClick} className={`toggle-button p-4 m-4 md:m-8 block`}>
       <span className={`underline underline-link`}>
-        {value ? 'Stop Showing' : 'Show'} {name}
+        {value ? 'stop showing' : 'show'} {name}
       </span>
     </button>
   );
