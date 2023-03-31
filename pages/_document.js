449c50d71a23d0c87848cd2501671640816ef810
Brett Farrow <brett@acorncity.com>
2023-03-30 22:31:18 -0600

    Fixed warnings

--- pages/_document.js
+++ pages/_document.js
@@ -5,14 +5,6 @@ class MyDocument extends Document {
     return (
       <Html className={`dark no-js`} lang="en">
         <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-          <meta
-            name="description"
-            content="A modified UI for Hacker News, starting with dark mode and no comments (by default). Not affiliated with Y Combinator or Hacker News in any way."
-          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
